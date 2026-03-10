fbc0995ebb8cacd65820c834c050b32a137a8018
brewcold <seung.yoon.yu@gmail.com>
2026-03-10 16:21:16 +0900

    클라이언트-서버 통신 테스트

--- core/client/src/routes/home.tsx
+++ core/client/src/routes/home.tsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from 'react-router'
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: '로센트 - 로스트아크 가격변동 알림' }, { name: 'description', content: '로스트아크 거래소 가격변동 알림' }]
 }
 
+export async function loader() {
+  const url = process.env.SERVER_ROOT!
+  const data = await fetch(`${url}/lostark`)
+  const text = await data.text()
+  return { text }
+}
+
 export default function Home() {
-  return <div>Locent</div>
+  const { text } = useLoaderData<typeof loader>()
+  return <div>{text}</div>
 }
